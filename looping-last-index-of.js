8e1d75815d7a799875f2c6738f8156fe732c5080
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-06-19 14:03:18 -0700

    implemented lastIndexOf func

---
+++ looping-last-index-of.js
@@ -0,0 +1,12 @@
+function lastIndexOf(arr, val) {
+    if(!arr.includes(val)) {
+        return -1;
+    }
+    return arr.length - arr.reverse().indexOf(val) - 1;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
